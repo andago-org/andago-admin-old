8adfce17e68adbaa026119efdb1500be66536aa7
Kottalo <kottaloflotenspieler@gmail.com>
2023-07-06 16:02:25 +0800

    Added Authing service

---
+++ src/services/authing.ts
@@ -0,0 +1,48 @@
+import { Authing as AuthingClient } from '@authing/browser';
+
+class Authing {
+  public client = new AuthingClient({
+    domain: 'https://andago.authing.cn',// 应用的认证地址
+    appId: '64a5715a3b3a89342064261e',// 应用 ID
+    redirectUri: 'http://localhost:3000',// 登录回调地址
+  })
+
+  public loginState: any = null
+
+  public async start()
+  {
+    if (this.client.isRedirectCallback()) {
+      console.log('redirect');
+
+      /**
+       * 以跳转方式打开 Authing 托管的登录页，认证成功后，
+       * 需要配合 handleRedirectCallback 方法，在回调端点处理 Authing 发送
+       * 的授权码或 token，获取用户登录态
+       */
+      this.client.handleRedirectCallback().then((res) => {
+        this.loginState = res;
+        window.location.replace('/')
+      });
+    } else {
+      await this.client.getLoginState()
+    }
+  }
+
+  public async login() {
+    await this.client.loginWithRedirect()
+  }
+
+  // public static async getLoginState() {
+  //   try {
+  //     this.loginState = await this.client.getLoginState()
+  //
+  //     console.log(this.loginState)
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+}
+
+const authing = new Authing()
+
+export default authing
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,51 +1,16 @@
 <template>
   <div>
-    <v-btn color="primary" @click="login">Login</v-btn>
+    <v-btn color="primary">Login</v-btn>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { Authing } from '@authing/browser';
+import authing from '@/services/authing'
 
-const loginState = ref(null as any)
 
-const authing = new Authing({
-  domain: 'https://andago.authing.cn',// 应用的认证地址
-  appId: '64a5715a3b3a89342064261e',// 应用 ID
-  redirectUri: 'https://localhost:3000',// 登录回调地址
+onMounted(async () => {
+  await authing.start()
 });
 
-onMounted(() => {
-
-
-  if (authing.isRedirectCallback()) {
-    console.log('redirect');
-
-    /**
-     * 以跳转方式打开 Authing 托管的登录页，认证成功后，
-     * 需要配合 handleRedirectCallback 方法，在回调端点处理 Authing 发送
-     * 的授权码或 token，获取用户登录态
-     */
-    authing.handleRedirectCallback().then((res) => {
-      loginState.value = res;
-      window.location.replace('/');
-    });
-  } else {
-    getLoginState();
-  }
-});
-
-function login() {
-  authing.loginWithRedirect();
-}
-
-async function getLoginState() {
-  try {
-    const state = await authing.getLoginState();
-    loginState.value = state;
-  } catch (error) {
-    console.log(error);
-  }
-}
 </script>
