35fd6b3dfdf14442ba7acdd6a7952443641e1735
Kottalo <kottaloflotenspieler@gmail.com>
2023-10-11 19:47:26 +0800

    Fixed

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,6 +6,7 @@
           <v-text-field label="Min Topup Amount" v-model="config.min_topup_amount" prefix="RM" validate-on="input"></v-text-field>
           <v-text-field label="Min Fare"  v-model="config.min_fare" prefix="RM"></v-text-field>
           <v-text-field label="Distance Add-on (per km)" v-model="config.fare_per_km" suffix="km"></v-text-field>
+          <v-text-field label="Duration Add-on (per min)" v-model="config.fare_per_min" suffix="min"></v-text-field>
           <v-text-field label="Search drivers from range (km)" v-model="config.min_range_km" suffix="km"></v-text-field>
           <v-text-field label="Search drivers to range (km)" v-model="config.max_range_km" suffix="km"></v-text-field>
           <v-text-field label="Range increase interval (sec)" v-model="config.range_increase_sec" suffix="sec"></v-text-field>
@@ -34,6 +35,7 @@ const config = reactive({
   min_topup_amount: 0,
   min_fare: 0,
   fare_per_km: 0,
+  fare_per_min: 0,
   min_range_km: 0,
   max_range_km: 0,
   range_increase_sec: 0,
